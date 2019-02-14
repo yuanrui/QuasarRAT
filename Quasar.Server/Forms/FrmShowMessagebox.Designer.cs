f2e126739a1dd5d7ad58a8de6b1232fcd87b54cf
yuanrui <yuanrui@live.cn>
2019-02-14 17:30:48 +0800

    update resource file

--- Quasar.Server/Forms/FrmShowMessagebox.Designer.cs
+++ Quasar.Server/Forms/FrmShowMessagebox.Designer.cs
@@ -45,6 +45,7 @@
             // 
             // groupMsgSettings
             // 
+            resources.ApplyResources(this.groupMsgSettings, "groupMsgSettings");
             this.groupMsgSettings.Controls.Add(this.cmbMsgIcon);
             this.groupMsgSettings.Controls.Add(this.lblMsgIcon);
             this.groupMsgSettings.Controls.Add(this.cmbMsgButtons);
@@ -53,121 +54,78 @@
             this.groupMsgSettings.Controls.Add(this.txtCaption);
             this.groupMsgSettings.Controls.Add(this.lblText);
             this.groupMsgSettings.Controls.Add(this.lblCaption);
-            this.groupMsgSettings.Location = new System.Drawing.Point(12, 12);
             this.groupMsgSettings.Name = "groupMsgSettings";
-            this.groupMsgSettings.Size = new System.Drawing.Size(325, 146);
-            this.groupMsgSettings.TabIndex = 0;
             this.groupMsgSettings.TabStop = false;
-            this.groupMsgSettings.Text = "Messagebox Settings";
             // 
             // cmbMsgIcon
             // 
+            resources.ApplyResources(this.cmbMsgIcon, "cmbMsgIcon");
             this.cmbMsgIcon.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbMsgIcon.FormattingEnabled = true;
-            this.cmbMsgIcon.Location = new System.Drawing.Point(147, 107);
             this.cmbMsgIcon.Name = "cmbMsgIcon";
-            this.cmbMsgIcon.Size = new System.Drawing.Size(162, 21);
-            this.cmbMsgIcon.TabIndex = 8;
             // 
             // lblMsgIcon
             // 
-            this.lblMsgIcon.AutoSize = true;
-            this.lblMsgIcon.Location = new System.Drawing.Point(42, 110);
+            resources.ApplyResources(this.lblMsgIcon, "lblMsgIcon");
             this.lblMsgIcon.Name = "lblMsgIcon";
-            this.lblMsgIcon.Size = new System.Drawing.Size(99, 13);
-            this.lblMsgIcon.TabIndex = 7;
-            this.lblMsgIcon.Text = "Messagebox Icon:";
             // 
             // cmbMsgButtons
             // 
+            resources.ApplyResources(this.cmbMsgButtons, "cmbMsgButtons");
             this.cmbMsgButtons.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbMsgButtons.FormattingEnabled = true;
-            this.cmbMsgButtons.Location = new System.Drawing.Point(147, 80);
             this.cmbMsgButtons.Name = "cmbMsgButtons";
-            this.cmbMsgButtons.Size = new System.Drawing.Size(162, 21);
-            this.cmbMsgButtons.TabIndex = 6;
             // 
             // lblMsgButtons
             // 
-            this.lblMsgButtons.AutoSize = true;
-            this.lblMsgButtons.Location = new System.Drawing.Point(23, 83);
+            resources.ApplyResources(this.lblMsgButtons, "lblMsgButtons");
             this.lblMsgButtons.Name = "lblMsgButtons";
-            this.lblMsgButtons.Size = new System.Drawing.Size(118, 13);
-            this.lblMsgButtons.TabIndex = 5;
-            this.lblMsgButtons.Text = "Messagebox Buttons:";
             // 
             // txtText
             // 
-            this.txtText.Location = new System.Drawing.Point(60, 49);
-            this.txtText.MaxLength = 256;
+            resources.ApplyResources(this.txtText, "txtText");
             this.txtText.Name = "txtText";
-            this.txtText.Size = new System.Drawing.Size(249, 22);
-            this.txtText.TabIndex = 4;
-            this.txtText.Text = "You are running Quasar.";
             // 
             // txtCaption
             // 
-            this.txtCaption.Location = new System.Drawing.Point(60, 21);
-            this.txtCaption.MaxLength = 256;
+            resources.ApplyResources(this.txtCaption, "txtCaption");
             this.txtCaption.Name = "txtCaption";
-            this.txtCaption.Size = new System.Drawing.Size(249, 22);
-            this.txtCaption.TabIndex = 2;
-            this.txtCaption.Text = "Information";
             // 
             // lblText
             // 
-            this.lblText.AutoSize = true;
-            this.lblText.Location = new System.Drawing.Point(24, 52);
+            resources.ApplyResources(this.lblText, "lblText");
             this.lblText.Name = "lblText";
-            this.lblText.Size = new System.Drawing.Size(30, 13);
-            this.lblText.TabIndex = 3;
-            this.lblText.Text = "Text:";
             // 
             // lblCaption
             // 
-            this.lblCaption.AutoSize = true;
-            this.lblCaption.Location = new System.Drawing.Point(6, 24);
+            resources.ApplyResources(this.lblCaption, "lblCaption");
             this.lblCaption.Name = "lblCaption";
-            this.lblCaption.Size = new System.Drawing.Size(51, 13);
-            this.lblCaption.TabIndex = 1;
-            this.lblCaption.Text = "Caption:";
             // 
             // btnPreview
             // 
-            this.btnPreview.Location = new System.Drawing.Point(181, 164);
+            resources.ApplyResources(this.btnPreview, "btnPreview");
             this.btnPreview.Name = "btnPreview";
-            this.btnPreview.Size = new System.Drawing.Size(75, 23);
-            this.btnPreview.TabIndex = 1;
-            this.btnPreview.Text = "Preview";
             this.btnPreview.UseVisualStyleBackColor = true;
             this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(262, 164);
+            resources.ApplyResources(this.btnSend, "btnSend");
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 2;
-            this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // FrmShowMessagebox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(349, 199);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.btnPreview);
             this.Controls.Add(this.groupMsgSettings);
-            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmShowMessagebox";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Show Messagebox []";
             this.Load += new System.EventHandler(this.FrmShowMessagebox_Load);
             this.groupMsgSettings.ResumeLayout(false);
             this.groupMsgSettings.PerformLayout();
--- Quasar.Server/Forms/FrmStartupAdd.Designer.cs
+++ Quasar.Server/Forms/FrmStartupAdd.Designer.cs
@@ -51,108 +51,75 @@
             this.groupAutostartItem.Controls.Add(this.txtName);
             this.groupAutostartItem.Controls.Add(this.lblPath);
             this.groupAutostartItem.Controls.Add(this.lblName);
-            this.groupAutostartItem.Location = new System.Drawing.Point(12, 12);
+            resources.ApplyResources(this.groupAutostartItem, "groupAutostartItem");
             this.groupAutostartItem.Name = "groupAutostartItem";
-            this.groupAutostartItem.Size = new System.Drawing.Size(653, 105);
-            this.groupAutostartItem.TabIndex = 0;
             this.groupAutostartItem.TabStop = false;
-            this.groupAutostartItem.Text = "Autostart Item";
             // 
             // lblType
             // 
-            this.lblType.AutoSize = true;
-            this.lblType.Location = new System.Drawing.Point(35, 74);
+            resources.ApplyResources(this.lblType, "lblType");
             this.lblType.Name = "lblType";
-            this.lblType.Size = new System.Drawing.Size(33, 13);
-            this.lblType.TabIndex = 4;
-            this.lblType.Text = "Type:";
             // 
             // cmbType
             // 
             this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbType.FormattingEnabled = true;
-            this.cmbType.Location = new System.Drawing.Point(74, 71);
+            resources.ApplyResources(this.cmbType, "cmbType");
             this.cmbType.Name = "cmbType";
-            this.cmbType.Size = new System.Drawing.Size(573, 21);
-            this.cmbType.TabIndex = 5;
-            this.toolTip1.SetToolTip(this.cmbType, "Remote Type of Autostart Item.");
+            this.toolTip1.SetToolTip(this.cmbType, resources.GetString("cmbType.ToolTip"));
             // 
             // txtPath
             // 
-            this.txtPath.Location = new System.Drawing.Point(74, 43);
+            resources.ApplyResources(this.txtPath, "txtPath");
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(573, 22);
-            this.txtPath.TabIndex = 3;
-            this.toolTip1.SetToolTip(this.txtPath, "Remote Path to Autostart Item.");
+            this.toolTip1.SetToolTip(this.txtPath, resources.GetString("txtPath.ToolTip"));
             this.txtPath.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPath_KeyPress);
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(74, 15);
-            this.txtName.MaxLength = 64;
+            resources.ApplyResources(this.txtName, "txtName");
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(573, 22);
-            this.txtName.TabIndex = 1;
             this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
             // 
             // lblPath
             // 
-            this.lblPath.AutoSize = true;
-            this.lblPath.Location = new System.Drawing.Point(35, 46);
+            resources.ApplyResources(this.lblPath, "lblPath");
             this.lblPath.Name = "lblPath";
-            this.lblPath.Size = new System.Drawing.Size(33, 13);
-            this.lblPath.TabIndex = 2;
-            this.lblPath.Text = "Path:";
             // 
             // lblName
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(29, 18);
+            resources.ApplyResources(this.lblName, "lblName");
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(39, 13);
-            this.lblName.TabIndex = 0;
-            this.lblName.Text = "Name:";
             // 
             // btnAdd
             // 
             this.btnAdd.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnAdd.Location = new System.Drawing.Point(576, 123);
+            resources.ApplyResources(this.btnAdd, "btnAdd");
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(89, 23);
-            this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "&Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(449, 123);
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(89, 23);
-            this.btnCancel.TabIndex = 2;
-            this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // FrmAddToAutostart
+            // FrmStartupAdd
             // 
             this.AcceptButton = this.btnAdd;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(677, 158);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.groupAutostartItem);
-            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "FrmAddToAutostart";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Add to Autostart";
+            this.Name = "FrmStartupAdd";
             this.groupAutostartItem.ResumeLayout(false);
             this.groupAutostartItem.PerformLayout();
             this.ResumeLayout(false);
